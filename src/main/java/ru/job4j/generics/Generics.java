cd3e416b8595c500927fba39be822fabad52aba9
Povvor <Povvor@yandex.ru>
2025-08-16 01:18:07 +0300

    1. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,70 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics generics = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        generics.printObject(first);
+        generics.printObject(second);
+        generics.printObject(third);
+        System.out.println();
+
+        /*
+          Так как ограничение сверху,
+          убираем все что ниже Predator
+         */
+        generics.printBoundedWildCard(second);
+        generics.printBoundedWildCard(third);
+        System.out.println();
+
+        /*
+          Так как ограничение снизу,
+          убираем все что выше Predator
+         */
+        generics.printLowerBoundedWildCard(first);
+        generics.printLowerBoundedWildCard(second);
+    }
+
+        /*
+        Используем WildCard
+        Для любого типа
+       */
+    public void printObject(List<?> list) {
+        for (Iterator<?> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+        /*
+        Используем Bounded Wildcard
+        для ограничения сверху
+       */
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+        /*
+        Используем Lower Bounded Wildcard
+        для ограничения снизу
+       */
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> iterator = list.iterator(); iterator.hasNext();) {
+            Object next = iterator.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
