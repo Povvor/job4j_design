fda7c571a6b217fe953d59f60b14c715e009712f
Povvor <Povvor@yandex.ru>
2025-09-04 01:17:53 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,9 +8,29 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+
+    private static Path path;
+    private static String fileExtension;
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        parseArgs(args);
+        Path start = path;
+        search(start, path -> path.toFile().getName().endsWith(fileExtension)).forEach(System.out::println);
+    }
+
+    private static void parseArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments. Number must be 2.");
+        }
+        path = Paths.get(args[0]);
+        if (Files.notExists(path)) {
+            throw new IllegalArgumentException("Path does not exist.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Extension must start with '.'");
+        }
+        fileExtension = args[1];
+
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
