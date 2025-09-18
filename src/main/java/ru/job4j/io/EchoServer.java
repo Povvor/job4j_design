46d218f8edaeeedb00674ca07a7aab75c49bd909
Povvor <Povvor@yandex.ru>
2025-09-18 18:44:20 +0300

    2. Что такое Socket? [#4850]

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EchoServer {
+
+    public static Map<String, String> parseArgs(String string) {
+        Map<String, String> values = new HashMap<>();
+        string = string.substring(string.indexOf("?") + 1);
+        string = string.substring(0, string.indexOf(" "));
+        String[] split = string.split("&");
+        for (String string2 : split) {
+            String[] split2 = string2.split("=");
+            values.put(split2[0], split2[1]);
+        }
+        return values;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream output = socket.getOutputStream();
+                     BufferedReader input = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                        if (string.startsWith("GET")) {
+                            Map<String, String> values = parseArgs(string);
+                            for (Map.Entry<String, String> entry : values.entrySet()) {
+                                if (entry.getKey().startsWith("msg")) {
+                                    switch (entry.getValue()) {
+                                        case "Hello":
+                                            output.write("Hello".getBytes());
+                                            break;
+                                        case "Bye":
+                                            output.write("Bye".getBytes());
+                                            server.close();
+                                            break;
+                                        default:
+                                            output.write("Wrong message".getBytes());
+                                    }
+                                }
+                            }
+                        }
+                        System.out.println(string);
+                    }
+                    output.flush();
+                }
+            }
+        }
+    }
+}
